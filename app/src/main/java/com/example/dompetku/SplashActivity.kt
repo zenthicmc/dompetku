f6dedaaeba1828ae787064632fd3656f365820c7
Jagad R R <jagadraya388@gmail.com>
2022-12-11 16:21:23 +0700

    Created Splash Screen

---
+++ app/src/main/java/com/example/dompetku/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.dompetku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+@Suppress("DEPRECATION")
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
