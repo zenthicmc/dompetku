d7a83a2312b34d510ec0057b71c1aa3e06f06dcf
Jagad R R <jagadraya388@gmail.com>
2022-12-11 15:58:38 +0700

    update

--- app/src/main/java/com/example/dompetku/WelcomeActivity.kt
+++ app/src/main/java/com/example/dompetku/WelcomeActivity.kt
@@ -2,11 +2,10 @@ package com.example.dompetku
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
 }
