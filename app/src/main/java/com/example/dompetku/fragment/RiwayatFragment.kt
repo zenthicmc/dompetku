5cae164e4ba841267846cfd940c4668c434c6ab9
dvaa <93278631+Dava-033@users.noreply.github.com>
2022-12-13 23:31:49 +0700

    Membuat Halaman RiwayatFragment

---
+++ app/src/main/java/com/example/dompetku/fragment/RiwayatFragment.kt
@@ -0,0 +1,22 @@
+package com.example.dompetku.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dompetku.R
+
+
+class RiwayatFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_riwayat, container, false)
+    }
+
+}
