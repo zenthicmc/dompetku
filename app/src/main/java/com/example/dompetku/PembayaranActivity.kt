bd47d4cd5a115cc871c02eccdd1d19d34efee800
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2022-12-25 19:13:16 +0700

    Update PembayaranActivity.kt

--- app/src/main/java/com/example/dompetku/PembayaranActivity.kt
+++ app/src/main/java/com/example/dompetku/PembayaranActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class PembayaranActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pembayaran_listrik)
+        setContentView(R.layout.activity_pembayaran)
     }
 }
