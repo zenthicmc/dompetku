ad28e49ed86eb90cc4573756ed64fb933e2714ea
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2022-12-25 19:12:38 +0700

    chage file name

--- app/src/main/java/com/example/dompetku/PembayaranActivity.kt
+++ app/src/main/java/com/example/dompetku/PembayaranActivity.kt
@@ -3,7 +3,7 @@ package com.example.dompetku
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class PembayaranListrikActivity : AppCompatActivity() {
+class PembayaranActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pembayaran_listrik)
