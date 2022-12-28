0b7ae55292625e185cf8a54c7b04d10cb68bc477
dvaa <93278631+Dava-033@users.noreply.github.com>
2022-12-28 22:32:36 +0700

    Membuat Halaman DetailTransaksiActivity

---
+++ app/src/main/java/com/example/dompetku/DetailTransaksiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dompetku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTransaksiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_transaksi)
+    }
+}
